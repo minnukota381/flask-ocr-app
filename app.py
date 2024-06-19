5c98e3fe338046ec95f4581638fabb88f945c4e1
minnukota381 <minnukota381@gmail.com>
2024-06-19 14:02:38 +0530

    "Changed app.run() call to specify host and port"

--- app.py
+++ app.py
@@ -126,4 +126,4 @@ def index():
     return render_template('index.html', text=text, image_file=image_file_base64)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
