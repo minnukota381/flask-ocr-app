fbdd50cb707a1c73c488e47afc33579080276a98
minnukota381 <minnukota381@gmail.com>
2024-06-19 15:42:56 +0530

    "Updated app.run() to use environment variable PORT or default to 5000"

--- app.py
+++ app.py
@@ -126,4 +126,5 @@ def index():
     return render_template('index.html', text=text, image_file=image_file_base64)
 
 if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
